3f37fcd663ab0c7e128fd672b99d2ef7969089c9
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-19 17:02:49 +0000

    chore: create 6.3 starting point from 6.2 script

---
+++ src/exercise/06-extra-3.js
@@ -0,0 +1,113 @@
+// Fix "perf death by a thousand cuts"
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+import {
+  useForceRerender,
+  useDebouncedState,
+  AppGrid,
+} from '../utils'
+
+import {
+  AppProvider,
+  useAppState,
+  useAppDispatch
+} from './context/app/app.context'
+
+import {
+  DogProvider,
+  useDogState
+} from './context/dog.context'
+
+function Grid() {
+  const dispatch = useAppDispatch()
+  const [rows, setRows] = useDebouncedState(50)
+  const [columns, setColumns] = useDebouncedState(50)
+  const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
+  return (
+    <AppGrid
+      onUpdateGrid={updateGridData}
+      rows={rows}
+      handleRowsChange={setRows}
+      columns={columns}
+      handleColumnsChange={setColumns}
+      Cell={Cell}
+    />
+  )
+}
+Grid = React.memo(Grid)
+
+function Cell({ row, column }) {
+  const state = useAppState()
+  const cell = state.grid[row][column]
+
+  return <CellImpl cell={cell} row={row} column={column} />
+}
+Cell = React.memo(Cell)
+
+function CellImpl({cell, row, column}) {
+  const dispatch = useAppDispatch()
+  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+  
+  return (
+    <button
+      className="cell"
+      onClick={handleClick}
+      style={{
+        color: cell > 50 ? 'white' : 'black',
+        backgroundColor: `rgba(0, 0, 0, ${cell / 100})`,
+      }}
+    >
+      {Math.floor(cell)}
+    </button>
+  )
+}
+CellImpl = React.memo(CellImpl)
+
+function DogNameInput() {
+  const [dogName, setDogName] = useDogState()
+
+  function handleChange(event) {
+    setDogName(event.target.value)
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <label htmlFor="dogName">Dog Name</label>
+      <input
+        value={dogName}
+        onChange={handleChange}
+        id="dogName"
+        placeholder="Toto"
+      />
+      {dogName ? (
+        <div>
+          <strong>{dogName}</strong>, I've a feeling we're not in Kansas anymore
+        </div>
+      ) : null}
+    </form>
+  )
+}
+function App() {
+  const forceRerender = useForceRerender()
+  return (
+    <div className="grid-app">
+      <button onClick={forceRerender}>force rerender</button>
+      <AppProvider>
+        <div>
+          <DogProvider>
+            <DogNameInput />
+          </DogProvider>
+          <Grid />
+        </div>
+      </AppProvider>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-func-assign: 0,
+*/
